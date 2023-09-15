87ca8fbe43e44d3246877496fec72aed40f78f11
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-15 10:29:06 -0600

    all problems were solved

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,17 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
+
+
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
 
 
 
@@ -24,9 +29,10 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-
-
-
-
+const analysis = "The second step, is to determine what the code is supposed to do and finding out all of the requirements that the code is suppose to have."
+const design = "The third step, is designing the program in it's most basic form. This includes finding out the outline of how the code is going to run. The design should be the skeleton that the code is based off of."
+const implementation = "The forth step, is basically just to write the code and basic testing to make sure that the code runs correctly. This is where all of the planning is put into action. The better the design phase goes will drastically decrease the time needed to be spent in this phase."
+const testing = "The fifth step, is to test the code. This step is a more intensive testing then just making sure the code runs, a Quality Assurance department will usually do most of the work in this step. When issues are found the process will go back to a previous step in the SDLC to fix the problem. Integration is refering to when the code is launched or given to the customer."
+const maintain = "The last step is a continual step, after the code is up and running there will need to be further fixes and updates to the code. Since technology is continually progressing the code will need to be changed to ensure that the code does not become obsolete."
 
 
